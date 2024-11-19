ccdb9f2ec3bb55b9f4f8633c9b8c36a8d9286a4b
Jewan1120 <hawaii1468@gmail.com>
2024-11-20 00:31:40 +0900

    feat(Controller): 게임 시작 문구를 출력하는 로직 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,7 +1,26 @@
 package baseball.controller;
 
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameController {
 
+    private final OutputView outputView;
+    private final InputView inputView;
+    private final GameService service;
+
+    public GameController() {
+        this.outputView = new OutputView();
+        this.inputView = new InputView();
+        this.service = new GameService();
+    }
+
     public void run() {
+        printGameStart();
+    }
+
+    private void printGameStart() {
+        outputView.printGameStart();
     }
 }

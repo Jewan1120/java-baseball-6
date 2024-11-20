01bf0dc3234e86ac3336289cc5d6564807baaa9a
Jewan1120 <hawaii1468@gmail.com>
2024-11-20 10:57:32 +0900

    feat(View): 사용자로부터 재시작 여부를 입력받을 뷰 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.enums.Constants;
 import baseball.enums.ViewMessages;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -7,6 +8,12 @@ public class InputView {
 
     public String promptNumbers() {
         System.out.print(ViewMessages.RPOMPT_NUMBERS.getMessage());
+        return Console.readLine();
+    }
+
+    public String promptRestart() {
+        System.out.println(ViewMessages.PROMPT_RESTART.getMessage(Constants.RESTART_YES.getValue(),
+                Constants.RESTART_NO.getValue()));
         return Console.readLine();
     }
 }
